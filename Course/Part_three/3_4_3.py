c8028b521a28b494fff5b18ff7f5ab462cac4134
victoriakovalyova <vikakot0004@gmail.com>
2021-02-13 20:57:45 +0400

    Add 3_2_1, 3_2_2, 3_2_3, 3_4_1, 3_4_2, 3_4_3 in Part three

---
+++ Course/Part_three/3_4_3.py
@@ -0,0 +1,39 @@
+
+    #на вход принимается набор строк
+    #считать строку циклом
+    #создает список
+    #посчитать средний балл по предмтам
+    #вывести значения среднего балла циклом
+    #
+    #Петров;85;92;78
+    #Сидоров;100;88;94
+    #Иванов;58;72;85
+    #85.0
+     #71.666666667
+     #81.0 
+#84.0 
+#85.666666667
+with open("dataset_3363_4.txt", "r") as information, open("output.txt", "w") as output:
+    counter, sum_math, sum_physics, sum_russian_language = 0, 0, 0, 0
+    for line in information:
+        _list = line.strip().split(";")
+        average = (int(_list[1]) + int(_list[2]) + int(_list[3])) / 3
+        counter += 1
+        sum_math += int(_list[1])
+        sum_physics += int(_list[2])
+        sum_russian_language += int(_list[3])
+        output.write(str(average)+"\n")
+    output.write(str(sum_math / counter) + " " + str(sum_physics / counter) + " " + str(sum_russian_language / counter))
+    #dictionary = {}
+    #number = 0
+    #name = ""
+    #for i in _list:
+    #    for j in i:
+    #        if j.isalpha() == True:
+    #            name = j
+    #            dictionary[j]
+    #        else:
+    #            number = j
+    #            dictionary[name] += int(number)
+    #        print(dictionary)
+
